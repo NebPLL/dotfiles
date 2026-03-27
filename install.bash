997e530490504efac9794c359d34d585dffbde40
NebPLL <226660953+NebPLL@users.noreply.github.com>
2026-03-27 10:09:13 +0000

    Update install.bash

--- install.bash
+++ install.bash
@@ -1,15 +1,18 @@
+#!/bin/bash
+
 sudo pacman -S ghostty 
 sudo pacman -S hypr 
 sudo pacman -S waybar 
 sudo pacman -S nvim 
 sudo pacman -S wofi 
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-mv  ~/dotfiles/ghostty/ ~/.config/
-mv  ~/dotfiles/hypr/ ~/.config/
-mv  ~/dotfiles/waybar/ ~/.config/
-mv  ~/dotfiles/nvim/ ~/.config/
-mv  ~/dotfiles/wofi/ ~/.config/
+mv "$SCRIPT_DIR/ghostty" "$SCRIPT_DIR/../.config/"
+mv "$SCRIPT_DIR/hypr" "$SCRIPT_DIR/../.config/"
+mv "$SCRIPT_DIR/waybar" "$SCRIPT_DIR/../.config/"
+mv "$SCRIPT_DIR/nvim" "$SCRIPT_DIR/../.config/"
+mv "$SCRIPT_DIR/wofi" "$SCRIPT_DIR/../.config/"
 
-mv  ~/dotfiles/Everforest/ ~/Pictures/
-mv  ~/dotfiles/Wallpaper/ ~/Pictures/
+mv "$SCRIPT_DIR/Everforest" "$SCRIPT_DIR/../Pictures/"
+mv "$SCRIPT_DIR/Wallpaper" "$SCRIPT_DIR/../Pictures/"
